5270d78fabcb260cffde5ec10096c1da155bd3f8
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-12 20:53:27 +0500

    Created account link button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import LinkAccountButton from "@/components/link-account";
+
 export default async function Home() {
   return (
     <>
       <h1 className="mt-5 text-center text-8xl"> Hello world</h1>
+      <LinkAccountButton />
     </>
   );
 }
