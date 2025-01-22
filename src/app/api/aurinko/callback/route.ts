e776a69ba43eff5964b0b3b0103f2facc4808e3e
Muneeb Mughal <muneeb.devp@gmail.com>
2025-01-22 17:18:39 +0500

    added mail sync
    Used vercel function to initial sync emails in callback

--- src/app/api/aurinko/callback/route.ts
+++ src/app/api/aurinko/callback/route.ts
@@ -1,7 +1,9 @@
-import { exchangeCodeForAccessToken, getAccount } from "@/lib/aurinko";
+import { exchangeCodeForAccessToken, getAccountDetails } from "@/lib/aurinko";
 import { db } from "@/server/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
+import { waitUntil } from '@vercel/functions'
+import axios from "axios";
 
 export async function POST(req: Request) {
 
@@ -17,18 +19,18 @@ export async function GET(req: NextRequest) {
 
   if (status !== 'success') return NextResponse.json({ error: 'Failed to link account' }, { status: 500 });
 
-  console.log('Code is: ' + params.get('code'));
   const code = params.get('code');
   if (!code) return NextResponse.json({ error: 'Auth code not provided' }, { status: 400 });
 
   const token = await exchangeCodeForAccessToken(code);
   if (!token) return NextResponse.json({ error: 'Failed to exchange auth code' }, { status: 500 });
 
-  const accountDetails = await getAccount(token.accessToken);
+  const accountDetails = await getAccountDetails(token.accessToken);
+  const accountId = token.accountId.toString()
 
   await db.account.upsert({
     where: {
-      id: token.accountId.toString()
+      id: accountId
     },
     update: {
       accessToken: token.accessToken
@@ -43,7 +45,15 @@ export async function GET(req: NextRequest) {
   });
 
   // Sync emails
+  waitUntil(
+    axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/api/mail-sync`, {
+      accountId,
+      userId
+    }
+    ).then(resp => console.log(resp.data))
+      .catch(err => console.error(err))
+  )
 
-
-  return NextResponse.json({ msg: 'Hello world' })
+  // return NextResponse.json({ msg: 'Hello world' })
+  return NextResponse.redirect(new URL('/mail', req.url))
 }
